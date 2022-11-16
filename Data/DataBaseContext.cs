3716a5ea9eeca41905234bcecc5a4306daafc99a
Nana Appiah <appiahnana41@gmail.com>
2022-11-16 12:01:12 +0500

    Add files via upload

---
+++ Data/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApiProject.Models;
+
+namespace RestApiProject.Data
+{
+    public class DataBaseContext:DbContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<ClubMeeting> clubMeetings { get; set; }
+        public DbSet<Presentation> Presentations { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+
+        
+    }
+}
