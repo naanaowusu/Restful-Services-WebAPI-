837dbcb036a992fc43e13800cc48f01d32ca57d4
Nana Appiah <appiahnana41@gmail.com>
2022-11-16 11:53:44 +0500

    Add migration files

---
+++ Migrations/20221104192045_InitialCreate.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RestApiProject.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Author = table.Column<string>(type: "text", nullable: false),
+                    PagesCount = table.Column<int>(type: "integer", nullable: false),
+                    CoverURL = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Tags = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Speakers",
+                columns: table => new
+                {
+                    SpeakerId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speakers", x => x.SpeakerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Presentations",
+                columns: table => new
+                {
+                    PresentationId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BookStore = table.Column<int>(type: "integer", nullable: false),
+                    PresenationURL = table.Column<string>(type: "text", nullable: false),
+                    VideoURL = table.Column<string>(type: "text", nullable: false),
+                    BookReview = table.Column<string>(type: "text", nullable: false),
+                    BookId = table.Column<int>(type: "integer", nullable: false),
+                    SpeakerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Presentations", x => x.PresentationId);
+                    table.ForeignKey(
+                        name: "FK_Presentations_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Presentations_Speakers_SpeakerId",
+                        column: x => x.SpeakerId,
+                        principalTable: "Speakers",
+                        principalColumn: "SpeakerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "clubMeetings",
+                columns: table => new
+                {
+                    ClubMeetingId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MeetingDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PresentationId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clubMeetings", x => x.ClubMeetingId);
+                    table.ForeignKey(
+                        name: "FK_clubMeetings_Presentations_PresentationId",
+                        column: x => x.PresentationId,
+                        principalTable: "Presentations",
+                        principalColumn: "PresentationId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_clubMeetings_PresentationId",
+                table: "clubMeetings",
+                column: "PresentationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Presentations_BookId",
+                table: "Presentations",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Presentations_SpeakerId",
+                table: "Presentations",
+                column: "SpeakerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "clubMeetings");
+
+            migrationBuilder.DropTable(
+                name: "Presentations");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Speakers");
+        }
+    }
+}
